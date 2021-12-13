bdd901fbfa93c7443f21e8eb5b90c513e362f564
ski <ski@skis-MacBook-Pro.local>
2021-12-14 02:01:48 +0300

    first commit

---
+++ scripts/fund_and_withdraw.py
@@ -0,0 +1,18 @@
+from brownie import FundMe, config, network
+from scripts.helpful_scripts import get_account
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee() + 100
+    print(f"entry fee: {entrance_fee}")
+    fund_me.fund({"from": account, "value": entrance_fee})
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    fund_me.withdraw({'from': account})
+    
+def main():
+    fund()
+    withdraw()
